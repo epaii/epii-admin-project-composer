17c382a9db7dee43800fa7088f718c70f6d7fc24
MrRen <543169072@qq.com>
2019-01-09 11:00:14 +0800

    init

--- test/index.php
+++ test/index.php
@@ -6,6 +6,8 @@
  * Time: 9:16 AM
  */
 
+use app\pluseinit;
+
 require_once __DIR__ . "/../../vendor/epii_admin_project_vendor/autoload.php";
 
 (new \epii\admin\center\App())->setConfig(new pluseinit())->run();
