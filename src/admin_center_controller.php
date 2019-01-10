12aff491ca848b6b0661f60c322cc7882a57abf1
MrRen <543169072@qq.com>
2019-01-10 16:48:26 +0800

    init

--- src/admin_center_controller.php
+++ src/admin_center_controller.php
@@ -12,6 +12,7 @@ namespace epii\admin\center;
 use epii\admin\center\app\root;
 use epii\admin\center\libs\Tools;
 use epii\app\controller;
+use epii\cli\Args;
 use epii\server\App;
 use wangshouwei\session\Session;
 
@@ -19,9 +20,15 @@ class admin_center_controller extends controller
 {
     public function init()
     {
+        if (Args::getVal("_show_runner"))
+        {
+            echo get_class(App::getInstance()->getRunner()[0])."@".App::getInstance()->getRunner()[1];
+            exit;
+        }
         if ( (!Session::get("is_login")) &&  ( !( get_class(App::getInstance()->getRunner()[0]) ===root::class && App::getInstance()->getRunner()[1]==="start" ) ) ) {
             header("location:" . Tools::get_web_root());
         }
+
 
     }
 }
