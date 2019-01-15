aa50f0f3f4b21af3a9af45f16173872aa98425d6
MrRen <543169072@qq.com>
2019-01-15 10:54:15 +0800

    init

--- test/index.php
+++ test/index.php
@@ -6,11 +6,9 @@
  * Time: 9:16 AM
  */
 
-use epii\admin\ui\EpiiAdminUi;
+
 
 require_once __DIR__ . "/../../vendor/epii_admin_project_vendor/autoload.php";
-
-EpiiAdminUi::setBaseConfig(["static_url_pre"=>"http://ziyuan.this.jt/","version"=>time()]);
 
 
 
