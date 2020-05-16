5b50f5ee9f575431330ae443707fd57c01bbf50b
MrRen <543169072@qq.com>
2020-05-16 16:00:06 +0800

    init

--- src/view/addons/index.php
+++ src/view/addons/index.php
@@ -13,6 +13,8 @@
                      <th data-field="install" data-formatter="epiiFormatter.switch" data-align="center"> 安装状态</th>
                      <th data-field="" data-formatter="install" data-align="center"> 安装</th>
                      <th data-field="" data-formatter="status" data-align="center"> 状态</th>
+                   
+                     <th data-field="" data-formatter="config"    data-align="center"> 配置</th>
                  </tr>
              </thead>
          </table>
@@ -24,7 +26,14 @@
          if (row.install) {
              return "已经安装";
          } else {
-             return "<a class='btn btn-outline-info btn-sm btn-confirm' data-ajax='1' data-msg=\"确定安装吗?\"  href='?app=addons@install&_vendor=1&name=" + row.name + "+'>安装</a>";
+             return "<a class='btn btn-outline-info btn-sm btn-confirm' data-ajax='1' data-msg=\"确定安装吗?\"  href='?app=addons@install&_vendor=1&name=" + row.name + "'>安装</a>";
+         }
+     }
+     function config(field_value, row, index, field_name) {
+         if (!row.install) {
+             return "未安装";
+         } else {
+             return "<a class='btn btn-outline-info btn-sm btn-dialog' data-area='90%,90%' data-title='配置'   href='?app=config&_vendor=1&addons_id=" + row.__data.id +"'>配置</a>";
          }
      }
      function status(field_value, row, index, field_name) {
@@ -32,9 +41,9 @@
              return "未安装";
          } 
          if (row.status-1==0) {
-            return "<a class='btn btn-outline-danger btn-sm btn-confirm' data-ajax='1' data-msg=\"确定关闭吗?\"  href='?app=addons@status&status=0&_vendor=1&name=" + row.name + "+'>关闭</a>";
+            return "<a class='btn btn-outline-danger btn-sm btn-confirm' data-ajax='1' data-msg=\"确定关闭吗?\"  href='?app=addons@status&status=0&_vendor=1&name=" + row.name + "'>关闭</a>";
          } else {
-             return "<a class='btn btn-outline-info btn-sm btn-confirm' data-ajax='1' data-msg=\"确定开启吗?\"  href='?app=addons@status&status=1&_vendor=1&name=" + row.name + "+'>开启</a>";
+             return "<a class='btn btn-outline-info btn-sm btn-confirm' data-ajax='1' data-msg=\"确定开启吗?\"  href='?app=addons@status&status=1&_vendor=1&name=" + row.name + "'>开启</a>";
          }
      }
  </script>
