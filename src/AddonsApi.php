1077f857df556decebbd2af77b00ddd501c800dd
MrRen <543169072@qq.com>
2020-11-09 09:30:27 +0800

    init

---
+++ src/AddonsApi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace epii\admin\center;
+
+use epii\admin\center\traits\addons_controller;
+
+class AddonsApi  extends Api
+{
+    use addons_controller;
+}
--- src/addons_controller.php
+++ src/addons_controller.php
@@ -4,8 +4,9 @@
 namespace epii\admin\center;
 
 use epii\admin\center\traits\addons_controller as addons_controller_base;
+use epii\app\controller;
 
-class addons_controller extends admin_center_controller
+class addons_controller extends controller
 {
     use addons_controller_base;
 }
